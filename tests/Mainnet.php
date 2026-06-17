d37ef2ee7cdc4e4d8327d4ab54ea905717d3ed90
leprechaun <leprechaun@steemfiles.com>
2026-06-17 13:11:47 -0300

    * simplify

--- tests/Mainnet.php
+++ tests/Mainnet.php
@@ -6,21 +6,11 @@ class Mainnet extends TestCase
 {
     // Shared, single initialization for all TestCase instances
     protected static $ec;
-    protected static $ch;
-    
-    private static $instanceCount = 0;
-    private static $electronCashDaemonProcess;
-    private static $bitcoinDaemonProcess;
+   
     public static function setUpBeforeClass(): void
     {
         $home = getenv('HOME') ?: (isset($_SERVER['HOME']) ? $_SERVER['HOME'] : null);
-        // start electron-cash daemon in regtest mode for tests
-        self::$electronCashDaemonProcess = exec('electron-cash daemon start > /dev/null 2>&1');
-        //self::$bitcoinDaemonProcess = exec('bitcoind -conf=bitcoin.conf -daemon > /dev/null 2>&1');
-        // give the daemon a moment to start
-
-        self::$ec = new ElectronCashRPC(null, "$home/.electron-cash/config");
-        echo "Mainnet: setUpBeforeClass: Started Electron Cash daemon and Bitcoin daemon\n";
+        self::$ec = new ElectronCashRPC();
         self::assertNotNull(self::$ec, "Failed to create ElectronCashRPC instance");
     }
 
@@ -29,9 +19,5 @@ class Mainnet extends TestCase
 
     public static function tearDownAfterClass(): void
     {
-            echo "Mainnet: Stopping Electron Cash daemon and Bitcoin daemon\n";
-            // stop the daemon processes
-            exec('electron-cash --regtest daemon stop > /dev/null 2>&1');
-            //exec('bitcoin-cli -conf=bitcoin.conf stop > /dev/null 2>&1');
     }
 }
